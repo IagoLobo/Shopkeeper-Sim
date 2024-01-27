5f38fa67a0cb47740627de7d646ff5fa284eeb0f
Iago Lobo <lobo.r.moraes@gmail.com>
2024-01-27 16:22:49 -0300

    Your Money text updates in Shop Menu

--- ShopkeeperSim/Assets/Scripts/Menus/ShopMenuController.cs
+++ ShopkeeperSim/Assets/Scripts/Menus/ShopMenuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShopMenuController : MonoBehaviour
@@ -8,10 +9,19 @@ public class ShopMenuController : MonoBehaviour
 
     public bool IsShopMenuOpen {get; private set;}
 
+    [Header("Canvas")]
     [SerializeField] private GameObject m_shopMenuCanvas;
+
+    [Header("Welcome Screen")]
     [SerializeField] private GameObject m_welcomeScreen;
+
+    [Header("Buy Screen")]
     [SerializeField] private GameObject m_buyScreen;
+    [SerializeField] private TextMeshProUGUI m_yourMoneyBuyScreenText;
+
+    [Header("Sell Screen")]
     [SerializeField] private GameObject m_sellScreen;
+    [SerializeField] private TextMeshProUGUI m_yourMoneySellScreenText;
 
     private void Awake()
     {
@@ -39,6 +49,11 @@ public class ShopMenuController : MonoBehaviour
         m_welcomeScreen.SetActive(!activate);
         m_buyScreen.SetActive(activate);
         m_sellScreen.SetActive(!activate);
+
+        if (activate)
+        {
+            UpdateMoneyText(m_yourMoneyBuyScreenText);
+        }
     }
 
     public void ActivateSellScreen(bool activate)
@@ -46,6 +61,11 @@ public class ShopMenuController : MonoBehaviour
         m_welcomeScreen.SetActive(!activate);
         m_buyScreen.SetActive(!activate);
         m_sellScreen.SetActive(activate);
+
+        if(activate)
+        {
+            UpdateMoneyText(m_yourMoneySellScreenText);
+        }
     }
 
     public void ActivateWelcomeScreen(bool activate)
@@ -53,5 +73,10 @@ public class ShopMenuController : MonoBehaviour
         m_welcomeScreen.SetActive(activate);
         m_buyScreen.SetActive(!activate);
         m_sellScreen.SetActive(!activate);
+    }
+
+    private void UpdateMoneyText(TextMeshProUGUI textRef)
+    {
+        textRef.text = InventoryManager.Instance.PlayerMoney + " G";
     }
 }

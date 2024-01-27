dc8a8d308ab9f55a857343d6956b165aabdbb76a
Iago Lobo <lobo.r.moraes@gmail.com>
2024-01-27 15:04:30 -0300

    Simple screen activations for shopkeeper

--- ShopkeeperSim/Assets/Scripts/Menus/ShopMenuController.cs
+++ ShopkeeperSim/Assets/Scripts/Menus/ShopMenuController.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class ShopMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject m_shopMenuCanvas;
+    [SerializeField] private GameObject m_welcomeScreen;
+    [SerializeField] private GameObject m_buyScreen;
+    [SerializeField] private GameObject m_sellScreen;
 
     public void ActivateShopMenu(bool activate)
     {
         m_shopMenuCanvas.SetActive(activate);
+    }
+
+    public void ActivateBuyScreen(bool activate)
+    {
+        m_welcomeScreen.SetActive(!activate);
+        m_buyScreen.SetActive(activate);
+        m_sellScreen.SetActive(!activate);
+    }
+
+    public void ActivateSellScreen(bool activate)
+    {
+        m_welcomeScreen.SetActive(!activate);
+        m_buyScreen.SetActive(!activate);
+        m_sellScreen.SetActive(activate);
+    }
+
+    public void ActivateWelcomeScreen(bool activate)
+    {
+        m_welcomeScreen.SetActive(activate);
+        m_buyScreen.SetActive(!activate);
+        m_sellScreen.SetActive(!activate);
     }
 }
